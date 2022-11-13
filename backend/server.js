1306a71792eb4d47d0af1fbc566d3d9a08e8972a
hazycora <hazycora@riseup.net>
2022-11-13 01:21:25 -0600

    Use "domain" query param and 400 HTTP status

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,32 @@ const utils = require("./util");
 const net = require("node:net");
 
 app.get("/api/lookup", async function(req, res) {
-  if (!req.query.query) {
-    res.send({
+  const queryDomain = req.query.domain
+  if (!queryDomain) {
+    res.status(400).json({
       success: false,
-      errorCode: "noQuery",
-      message: "No query was provided."
+      errorCode: "noQueryDomain",
+      message: "No domain was provided."
     });
     return;
   }
 
-  let isIp = net.isIP(req.query.query);
+  let isIp = net.isIP(queryDomain);
   if (isIp == 4 || isIp == 6) isIp = true;
   else isIp = false;
 
   try {
-    let lookup = await utils.lookup(req.query.query, isIp);
-    res.send({success: true, ...lookup});
+    let lookup = await utils.lookup(queryDomain, isIp);
+    res.json({
+      success: true,
+      ...lookup
+    });
   } catch(err) {
-    res.send({success: false, errorCode: "internal", message: (err.message || err.code)});
+    res.json({
+      success: false,
+      errorCode: "internal",
+      message: (err.message || err.code)
+    });
   }
 });
 
