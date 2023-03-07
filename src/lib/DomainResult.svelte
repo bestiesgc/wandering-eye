96c46c6880cce4e920fd2cd2b37c13d1bc64e413
hazycora <hazycora@riseup.net>
2023-03-07 00:06:06 -0600

    Refactor Domain' IP results to new component

--- src/lib/DomainResult.svelte
+++ src/lib/DomainResult.svelte
@@ -1,6 +1,6 @@
 <script>
 	export let domainResult
-	import Leaflet from './Leaflet.svelte'
+	import IpResult from './IpResult.svelte'
 </script>
 
 <div class="result-item">
@@ -48,36 +48,7 @@
 	<h3>IP Addresses</h3>
 	<div class="ip-container-list">
 		{#each domainResult.ipAddresses as ip}
-			<div class="ip-container" class:has-geo={ip.geo}>
-				{#if ip.geo}
-					<Leaflet ll={ip.geo.ll} />
-				{/if}
-				<div class="ip-meta">
-					<h4 class="ip-meta_address">{ip.value}</h4>
-					{#if ip.whois?.Organization}
-						<hr />
-						{#if ip.type}
-							<p><b>Type</b>: {ip.type}</p>
-						{/if}
-						{#if ip.hostname}
-							<p><b>Hostname(s)</b>: {ip.hostname}</p>
-						{/if}
-						<p><b>Organization</b>: {ip.whois.Organization}</p>
-						{#if ip.whois.Updated}
-							<p><b>Updated</b>: {ip.whois.Updated}</p>
-						{/if}
-						{#if ip.whois.contactAbuse?.OrgAbuseEmail}
-							<p><b>Abuse Contact</b>: {ip.whois.contactAbuse.OrgAbuseEmail}</p>
-						{/if}
-						{#if ip.abuse.abuseipdb}
-							<p><b>AbuseIPDB Reports</b>: {ip.abuse.abuseipdb}</p>
-						{/if}
-						{#if ip.abuse.abuseipdb}
-							<p><b>URLHaus Reports</b>: {ip.abuse.urlhaus}</p>
-						{/if}
-					{/if}
-				</div>
-			</div>
+			<IpResult {ip} />
 		{/each}
 	</div>
 </div>
---
+++ src/lib/IpResult.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Leaflet from './Leaflet.svelte'
+	export let ip
+</script>
+
+<div class="ip-container" class:has-geo={ip.geo}>
+	{#if ip.geo}
+		<Leaflet ll={ip.geo.ll} />
+	{/if}
+	<div class="ip-meta">
+		<h4 class="ip-meta_address">{ip.value}</h4>
+		{#if ip.whois?.Organization}
+			<hr />
+			{#if ip.type}
+				<p><b>Type</b>: {ip.type}</p>
+			{/if}
+			{#if ip.hostname}
+				<p><b>Hostname(s)</b>: {ip.hostname}</p>
+			{/if}
+			<p><b>Organization</b>: {ip.whois.Organization}</p>
+			{#if ip.whois.Updated}
+				<p><b>Updated</b>: {ip.whois.Updated}</p>
+			{/if}
+			{#if ip.whois.contactAbuse?.OrgAbuseEmail}
+				<p><b>Abuse Contact</b>: {ip.whois.contactAbuse.OrgAbuseEmail}</p>
+			{/if}
+			{#if ip.abuse.abuseipdb}
+				<p><b>AbuseIPDB Reports</b>: {ip.abuse.abuseipdb}</p>
+			{/if}
+			{#if ip.abuse.abuseipdb}
+				<p><b>URLHaus Reports</b>: {ip.abuse.urlhaus}</p>
+			{/if}
+		{/if}
+	</div>
+</div>
