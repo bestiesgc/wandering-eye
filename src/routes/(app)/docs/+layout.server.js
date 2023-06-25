434570dff427a72b456117e63e173724147cbb87
hazycora <hazysu@riseup.net>
2023-06-25 14:02:52 -0500

    refactor docs layout load function

--- src/routes/(app)/docs/+layout.server.js
+++ src/routes/(app)/docs/+layout.server.js
@@ -1,21 +1,26 @@
-const modules = import.meta.glob('./**/*.md')
-const files = Promise.all(
-	Object.entries(modules).map(async e => {
-		const page = await e[1]()
-		const path = e[0].replace(/^\.\//, '/docs/').replace(/\/\+page\.md$/, '')
-		return {
-			...page.metadata,
-			path: path
-		}
-	})
-)
+async function getArticles() {
+	const modules = import.meta.glob('./**/*.md')
+	const files = await Promise.all(
+		Object.entries(modules).map(async e => {
+			const page = await e[1]()
+			const path = e[0].replace(/^\.\//, '/docs/').replace(/\/\+page\.md$/, '')
+			return {
+				...page.metadata,
+				path: path
+			}
+		})
+	)
+	return files
+		.filter(article => !article.hidden)
+		.sort((a, b) => {
+			return new Date(b.date) - new Date(a.date)
+		})
+}
+
+const articles = await getArticles()
 
 export async function load() {
 	return {
-		articles: (await files)
-			.filter(article => !article.hidden)
-			.sort((a, b) => {
-				return new Date(b.date) - new Date(a.date)
-			})
+		articles
 	}
 }
