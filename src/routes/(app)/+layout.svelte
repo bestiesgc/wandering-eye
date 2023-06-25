67083f3e37035c11019bfac9ac18e342219711db
hazycora <hazysu@riseup.net>
2023-06-25 12:20:21 -0500

    add footer to app layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,22 +4,38 @@
 	import EyeIcon from '$lib/Icons/eye.svg'
 </script>
 
-<nav>
-	<a href="/" class="logo"><EyeIcon /></a>
-	<DomainSearch value={$page.data.query} />
-	<span class="end" />
-</nav>
-
-<div class="app">
-	<slot />
+<div class="layout">
+	<nav>
+		<a href="/" class="logo"><EyeIcon /></a>
+		<DomainSearch value={$page.data.query} />
+		<span class="end" />
+	</nav>
+	<div class="app">
+		<slot />
+	</div>
+	<footer>
+		service by <a href="https://besties.house">besties</a> -
+		<a href="https://git.gay/besties/wandering-eye">source</a>
+	</footer>
 </div>
 
 <style>
+	.layout {
+		min-height: 100vh;
+		min-height: 100svh;
+		display: grid;
+		grid-template-rows: min-content 1fr min-content;
+	}
 	nav {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		padding: 0.5rem;
+	}
+	footer {
+		padding: 0.5rem;
+		font-size: 0.8rem;
+		color: var(--grey-300);
 	}
 	.logo,
 	.end {
